442f675a7277bead6c1d0d71f581615b2d55cd72
Mathen Jose <mathen@hotmail.co.uk>
2020-08-28 16:54:29 +1000

    Setting up to query mongo

--- api/index.js
+++ api/index.js
@@ -7,7 +7,6 @@ process.on('uncaughtException', function (err) {
   console.log('Caught exception: ', err);
 });
 
-
 let express = require('express')
 let request = require('request-promise')
 let fs = require('fs')
@@ -20,6 +19,15 @@ let smarts = require('smarts')()
 
 let handleAttachments = require('functions/handleAttachments')
 
+
+// create mongoDB connection
+let MongoClient = require('mongodb').MongoClient;
+let uri = config.uri
+let client = new MongoClient(uri, { 
+	useNewUrlParser: true, 
+	useUnifiedTopology: true 
+});
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -29,21 +37,47 @@ app.get('/', (req, res) => res.send('Hello API World!'));
 
 app.post('/info', async (req,res)=>{
 	try {
-		
+		let keywords_included = false;
+		var return_message = "No info right now but we're working on it!";
 		let reqID = uuid()
 		
 		console.log("New req", req.body, reqID)
 
 		if(req.body.attachments && req.body.attachments instanceof Array){
 			await handleAttachments(req, res)
-		}
+		}else if (typeof req.body.message == "string") {
+			keywords_included = true;
+			return_message = "Keyword input recieved: " + req.body.message;
+			console.log("Querying the database with the following keyword(s): " + req.body.message);
+
+			// connect to mongodb connection
+			/*
+			client.connect(async err => {
+				if(err) console.error(err)
+				
+				let db = client.db("wastee");
+				let items = db.collection("items")
+
+				await asyncForEach(parsed, async item=>{
+					let found = await items.query(
+						// QUERY the db 
+						).catch(console.error)
+					console.log(found)
+				})
+				
+				
+				// close the connection
+				client.close();
+				*/
+			};
+		
 
 	} catch(err){
 		console.error(err)
 	}
 	
 	res.status(200).send({
-		info: "No info right now but we're working on it!"
+		info: return_message
 	})
 	
 })
