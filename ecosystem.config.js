62be2db3b40a4d20117cf7ab1a7646474f8b70fa
Nikolaj Frey <lopudesigns@gmail.com>
2020-08-30 21:19:52 +1000

    Added watch config to pm2 config file and removed deployment config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,26 +1,12 @@
 module.exports = {
-  apps : [{
-    name: 'wasteAPI',
-    script: 'api/index.js',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
-    }
-  }],
-  deploy : {
-    production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-    }
-  }
+  apps : [
+		{
+			name: 'wasteAPI',
+			script: 'api/index.js',
+			instances: 1,
+			autorestart: true,
+			watch: 'api/',
+			max_memory_restart: '1G',
+		}
+	]
 };
